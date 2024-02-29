b6bba44b22c9865c34c6b50127b5696fcf71af25
Fancy2209 <64917206+Fancy2209@users.noreply.github.com>
2024-02-29 11:33:04 -0100

    Create describeType.hx

---
+++ src/openfl/utils/describeType.hx
@@ -0,0 +1,31 @@
+package openfl.utils;
+public function describeType(value:*):XML {
+  var type:Class = value.$isClass ? value : value.self;
+  // fake collection:
+  var len:uint = 0;
+  var methods:Object = {
+    length: function():uint {
+      return len;
+    }
+  };
+  if (type && type.prototype) {
+    var propertyNames:Array = Object.getOwnPropertyNames(type.prototype);
+    for each (var p:String in propertyNames) {
+      if (p.match(/^[a-zA-Z_]/)) {
+        var descriptor:* = Object.getOwnPropertyDescriptor(type.prototype, p);
+        if (typeof descriptor.value === "function") {
+          methods[len++] = p;
+        }
+      }
+    }
+  }
+  var result:* = {
+    attribute: function(attr:String):String {
+      return attr == "name" ? getQualifiedClassName(value) : null;
+    },
+    method: {
+      "@name": methods
+    }
+  };
+  return result;
+}
