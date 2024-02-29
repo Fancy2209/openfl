ff28cb4c2f22e3ff0eb5bc3831f769f185fed5ec
Fancy2209 <64917206+Fancy2209@users.noreply.github.com>
2024-02-29 11:47:15 -0100

    Update describeType.hx

--- src/openfl/utils/describeType.hx
+++ src/openfl/utils/describeType.hx
@@ -1,11 +1,11 @@
 package openfl.utils;
 //Based on JooFlash https://github.com/CoreMedia/jangaroo-libs/blob/jooflash-4.1/jooflash/src/main/joo/flash/utils/describeType.as
-public function describeType(value:*):XML {
+public function describeType(value):XML {
   var type:Class = value.$isClass ? value : value.self;
   // fake collection:
-  var len:uint = 0;
+  var len:Int = 0;
   var methods:Object = {
-    length: function():uint {
+    length: function():Int {
       return len;
     }
   };
@@ -13,14 +13,14 @@ public function describeType(value:*):XML {
     var propertyNames:Array = Object.getOwnPropertyNames(type.prototype);
     for each (var p:String in propertyNames) {
       if (p.match(/^[a-zA-Z_]/)) {
-        var descriptor:* = Object.getOwnPropertyDescriptor(type.prototype, p);
+        var descriptor = Object.getOwnPropertyDescriptor(type.prototype, p);
         if (typeof descriptor.value === "function") {
           methods[len++] = p;
         }
       }
     }
   }
-  var result:* = {
+  var result = {
     attribute: function(attr:String):String {
       return attr == "name" ? getQualifiedClassName(value) : null;
     },
